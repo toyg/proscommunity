a7101b7fdbeb9011988acf21ae2fe4ae350f99d1
Giacomo Lacava <g.lacava@gmail.com>
2023-04-01 10:19:10 +0100

    better style for options

--- chrome/options.js
+++ chrome/options.js
@@ -1,11 +1,11 @@
 // Saves options to chrome.storage
 const saveOptions = function() {
-  let autologinValue = document.getElementById('autologin').checked;
+  let autologinValue = document.getElementById('pros-autologin').checked;
   chrome.storage.sync.set(
     { autologin: autologinValue },
     function() {
       // Update status to let user know options were saved.
-      let status = document.getElementById('status');
+      let status = document.getElementById('pros-optionstatus');
       status.textContent = 'Options saved.';
       setTimeout(() => {
         status.textContent = '';
@@ -20,9 +20,9 @@ const restoreOptions = function(){
   chrome.storage.sync.get(
     { autologin: false },
     function(items) {
-      document.getElementById('autologin').checked = items.autologin;
+      document.getElementById('pros-autologin').checked = items.autologin;
     }
   );
 };
 document.addEventListener('DOMContentLoaded', restoreOptions);
-document.getElementById('autologin').addEventListener('click', saveOptions);
+document.getElementById('pros-autologin').addEventListener('click', saveOptions);
