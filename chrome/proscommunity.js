f40cafa79a7fff43a0ddd67f5734fb284a598d85
Giacomo Lacava <g.lacava@gmail.com>
2023-04-24 22:04:47 +0100

    - fix error when monitored node does not exist

--- chrome/proscommunity.js
+++ chrome/proscommunity.js
@@ -86,28 +86,29 @@ if(noReplied != null){
 // Select the node that will be observed for mutations
 const targetNode = document.querySelector("div.message-list");
 
-// Options for the observer (which mutations to observe)
-const config = { attributes: false, childList: true, subtree: false };
+if(targetNode) {
+    // Options for the observer (which mutations to observe)
+    const config = { attributes: false, childList: true, subtree: false };
 
-// Callback function to execute when mutations are observed
-const callback = (mutationList, observer) => {
-    for (const mutation of mutationList) {
-        if (mutation.type === "childList") {
-            mutation.addedNodes.forEach(n => {
-                if(n.nodeName.toLowerCase() == "article") {
-                    modNode(n);
-                }
-            })
+    // Callback function to execute when mutations are observed
+    const callback = (mutationList, observer) => {
+        for (const mutation of mutationList) {
+            if (mutation.type === "childList") {
+                mutation.addedNodes.forEach(n => {
+                    if(n.nodeName.toLowerCase() == "article") {
+                        modNode(n);
+                    }
+                })
+            }
         }
-    }
-};
+    };
 
-// Create an observer instance linked to the callback function
-const observer = new MutationObserver(callback);
+    // Create an observer instance linked to the callback function
+    const observer = new MutationObserver(callback);
 
-// Start observing the target node for configured mutations
-observer.observe(targetNode, config);
+    // Start observing the target node for configured mutations
+    observer.observe(targetNode, config);
 
-// Later, you can stop observing
-//observer.disconnect();
-
+    // Later, you can stop observing
+    //observer.disconnect();
+}
