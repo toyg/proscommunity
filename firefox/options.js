8831da916abd7ddf5ebf6aae83495b697da70cfd
Giacomo Lacava <g.lacava@gmail.com>
2023-10-17 10:32:10 +0100

    Done options for notifications

--- firefox/options.js
+++ firefox/options.js
@@ -1,3 +1,8 @@
+const enableMonitorNodeId = 'pros-monitorEnabled';
+const enableAutologinNodeId = 'pros-autologin';
+const inputContainerId = 'pros-subs';
+const messageNodeId = 'pros-optionstatus';
+
 // gets elements in the page that deal with subscriptions
 function getSubsItems(){
   return document.querySelectorAll(".pros-subs-subforum");
@@ -6,8 +11,8 @@ function getSubsItems(){
 // Saves options to chrome.storage
 const saveOptions = function() {
   // retrieve current values
-  let autologinValue = document.getElementById('pros-autologin').checked;
-  let monitorEnabledValue = document.getElementById('pros-monitorEnabled').checked;
+  let autologinValue = document.getElementById(enableAutologinNodeId).checked;
+  let monitorEnabledValue = document.getElementById(enableMonitorNodeId).checked;
   let subs = {};
   getSubsItems().forEach(function(currValue){
       subs[currValue.id] = currValue.checked;
@@ -19,7 +24,7 @@ const saveOptions = function() {
         subs: JSON.stringify(subs)
     }).then( function() {
       // Update status to let user know options were saved.
-      let status = document.getElementById('pros-optionstatus');
+      let status = document.getElementById(messageNodeId);
       status.textContent = 'Options saved.';
       setTimeout(() => {
         status.textContent = '';
@@ -34,8 +39,12 @@ const restoreOptions = function(){
   browser.storage.sync.get(
     { autologin: false, subs: "{}", monitorEnabled: false },
     function(items) {
-      document.getElementById('pros-autologin').checked = items.autologin;
-      document.getElementById('pros-monitorEnabled').checked = items.monitorEnabled;
+      document.getElementById(enableAutologinNodeId).checked = items.autologin;
+      let monitorInput = document.getElementById(enableMonitorNodeId);
+      monitorInput.checked = items.monitorEnabled;
+        var event = document.createEvent("HTMLEvents");
+        event.initEvent('change', false, true);
+        monitorInput.dispatchEvent(event);
       let parsedJs = JSON.parse(items.subs);
       getSubsItems().forEach(function(currValue) {
         currValue.checked = parsedJs[currValue.id];
@@ -43,12 +52,38 @@ const restoreOptions = function(){
     }
   );
 };
+
+// disable subs UI if global notification option is off
+function disableInputs(newState){
+    let nodes = document.getElementById(inputContainerId).getElementsByTagName('input');
+    for(let i = 0; i < nodes.length; i++){
+        nodes[i].disabled = newState;
+    }
+}
+const toggleSubs = function(){
+//    let inputContainer = 'pros-subs';
+    let disabledClass = 'disabled-opt';
+    if(document.getElementById(enableMonitorNodeId).checked){
+        document.getElementById(inputContainerId).classList.remove(disabledClass);
+        disableInputs(false);
+    } else {
+        document.getElementById(inputContainerId).classList.add(disabledClass);
+        disableInputs(true);
+    }
+}
+
+
+
 // main flow
 // restore options
 document.addEventListener('DOMContentLoaded', restoreOptions);
+document.addEventListener('DOMContentLoaded', toggleSubs);
 // trigger saving on any change
-document.getElementById('pros-autologin').addEventListener('click', saveOptions);
-document.getElementById('pros-monitorEnabled').addEventListener('click', saveOptions);
+document.getElementById(enableAutologinNodeId).addEventListener('click', saveOptions);
+document.getElementById(enableMonitorNodeId).addEventListener('click', saveOptions);
+document.getElementById(enableMonitorNodeId).addEventListener('change', toggleSubs);
+
 getSubsItems().forEach(function(currValue){
     currValue.addEventListener('click', saveOptions);
-})
+});
+
