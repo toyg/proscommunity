b4fdae49329657c31a3d66849e6dcade52ff6660
Giacomo Lacava <g@lacava.email>
2023-10-19 16:44:09 +0100

    implemented background as service worker on chrome. Bumping to 2.0.

---
+++ chrome/offscreen.js
@@ -0,0 +1,66 @@
+/* DOMParser is not available in service workers,
+so we use the offscreen page to use it.
+This script will execute when triggered by a message.
+*/
+
+const rssUrl = "https://community.onestreamsoftware.com/otqon28567/rss/Category?category.id=your-community&interaction.style=forum";
+const baseUrl = "https://community.onestreamsoftware.com/t5/"
+
+async function main(options) {
+
+    let lastChecked = Date.parse(options.lastChecked);
+    let subsList = options.subs;
+    const v = await fetch(rssUrl).then((t) => t.text()).then(xml => {
+        // parse XML
+        var parser = new DOMParser();
+        var doc = parser.parseFromString(xml, "text/xml");
+        // we don't check on first run, no point
+        if(lastChecked !== null){
+            // namespace machinery for xpath
+            const nsResolver = doc.createNSResolver(
+                doc.ownerDocument === null
+                    ? doc.documentElement
+                    : doc.ownerDocument.documentElement,
+            );
+
+            // prep forum link for notification
+            let forumLinkNode = doc.evaluate("/rss/channel/link", doc, null, XPathResult.FIRST_ORDERED_NODE_TYPE);
+            let forumLink = forumLinkNode.singleNodeValue.textContent;
+            // check if posts exist after the recorded timestamp
+            let posts = doc.evaluate("/rss/channel/item", doc, nsResolver, XPathResult.UNORDERED_NODE_ITERATOR_TYPE);
+            let post = null;
+            while(post = posts.iterateNext()){
+                let postLink = post.getElementsByTagName("link")[0].textContent;
+                let postDate = new Date( Date.parse(post.getElementsByTagName("pubDate")[0].textContent));
+                if((postDate >= lastChecked) && subsList.some(boardUrl => postLink.startsWith(boardUrl))) {
+                    chrome.runtime.sendMessage(
+                        JSON.stringify(
+                            {"action": "NEW_POSTS",
+                            "data": forumLink}
+                        )
+                    );
+                    break;
+                }
+            }
+        }
+    })
+    .catch(function(err) {
+        console.log('Failed to fetch page: ', err);
+    });
+}
+
+// hook execution to receiving messages
+chrome.runtime.onMessage.addListener(async (msg) => {
+    let options = JSON.parse(msg);
+    //console.log(options);
+    switch(options.action){
+        case "PING":
+            chrome.runtime.sendMessage(JSON.stringify({"action": "PONG"}));
+            break;
+        case "RUN":
+            main(options);
+            break;
+        default:
+            console.log("weird message", msg);
+    };
+});
---
+++ chrome/proscommunity-bkgd.js
@@ -0,0 +1,111 @@
+console.log("[PROSCommunity] Starting up...");
+
+// setup offscreen page to use DOM Parser (screw you, Google)
+chrome.offscreen.createDocument({
+    url: chrome.runtime.getURL('offscreen.html'),
+    reasons: [chrome.offscreen.Reason.DOM_PARSER],
+    justification: 'fetching rss to check for new posts',
+}).catch((err) => {} ) // if we have it already, skip
+
+// keep a ping going every 25 seconds to keep the service up
+setInterval(() => {
+    chrome.runtime.sendMessage(JSON.stringify({"action": "PING"}));
+}, 25000);
+
+// wrap all flow in an async function to satisfy Chrome developer's wankery
+async function run() {
+
+var lastChecked = null;
+var isMonitorEnabled = false;
+var subsList = [];
+
+const baseUrl = "https://community.onestreamsoftware.com/t5/"
+const rssUrl = "https://community.onestreamsoftware.com/otqon28567/rss/Category?category.id=your-community&interaction.style=forum"
+
+// DEBUG uncomment this to test notifications
+//chrome.storage.sync.set({lastChecked: "2022-10-16T17:39:10Z"});
+// END DEBUG
+
+// retrieve last-checked timestamp
+const initLastChecked = chrome.storage.sync.get(["lastChecked"]).then((options) => {
+    try{
+        let lcTimestamp = new Date(Date.parse(options.lastChecked));
+        if(lcTimestamp.toString() != "Invalid Date") lastChecked = lcTimestamp;
+    }catch(error){}
+    }
+);
+
+// retrieve options
+function refreshOptions(options){
+    isMonitorEnabled = options.monitorEnabled;
+    let subsObj = JSON.parse(options.subs);
+    // subsObj -> {"Rules": true, ...}
+    // looping through properties with .entries returns arrays: [ ["Rules", true], ...]
+    // so we filter the enabled ones, then retrieve their names and attach each of them to baseUrl
+    subsList = Object.entries(subsObj).filter(x => {return x[1]}).map(x => {return baseUrl + x[0]});
+}
+const initMonitor = chrome.storage.sync.get(["monitorEnabled", "subs"]).then((options) => {
+   refreshOptions(options);
+});
+
+// init
+await initLastChecked;
+await initMonitor;
+
+/* create an alarm, then attach our recurring action to it */
+
+// Check every x minutes
+chrome.alarms.create({delayInMinutes: 1, periodInMinutes: 30});
+console.log("[PROSCommunity] monitor enabled: " + isMonitorEnabled);
+console.log("[PROSCommunity] monitor last checked: " + lastChecked);
+
+if(isMonitorEnabled == true) {
+    chrome.alarms.onAlarm.addListener(() => {
+        // we always refresh options before actually fetching and filtering
+        chrome.storage.sync.get(["monitorEnabled", "subs"]).then((options) => {
+            refreshOptions(options);
+            if(isMonitorEnabled){
+            // console.log("sending message ", lastChecked, subsList);
+                // send message to offscreen page to trigger fetching
+                chrome.runtime.sendMessage(JSON.stringify(
+                    {"action": "RUN",
+                    "lastChecked": lastChecked.toString(),
+                    "subs": subsList}
+                ));
+                // update lastChecked
+                chrome.storage.sync.set({lastChecked: Date().toString()});
+            }
+        })
+    });
+}
+
+}
+
+// if receiving a message, fire a notification
+chrome.runtime.onMessage.addListener((msg) => {
+    let options = JSON.parse(msg);
+    //console.log(options);
+    switch(options.action){
+        case "PONG":
+            // ignore
+            break;
+        case "NEW_POSTS":
+            // notification firing
+            chrome.notifications.create("newposts", {
+              type: "basic",
+              iconUrl: chrome.runtime.getURL("images/pros48.png"),
+              title: "New OS threads",
+              message: "There are new posts on your OneCommunity boards!"
+            });
+            // when clicking on the popup, user must be sent to forums
+            chrome.notifications.onClicked.addListener(function(notificationId) {
+                chrome.tabs.create({url: options.data});
+                chrome.notifications.clear(notificationId);
+            });
+            break;
+        default:
+            console.log("weird message", msg);
+    };
+})
+
+run();
